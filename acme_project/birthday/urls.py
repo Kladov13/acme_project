345a367595cec9042a03beeed9f43c6c51771e7b
Kladov13 <kladov113@yandex.com>
2024-07-21 13:42:42 +0300

    Continue Django

--- acme_project/birthday/urls.py
+++ acme_project/birthday/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from django.conf import settings
+
+from django.conf.urls.static import static
+
+from django.contrib import admin
+
 from . import views
 
 app_name = 'birthday'
@@ -7,4 +13,6 @@ app_name = 'birthday'
 urlpatterns = [
     path('', views.birthday, name='create'),
     path('list/', views.birthday_list, name='list'),
-]
+    path('<int:pk>/edit/', views.birthday, name='edit'),
+    path('<int:pk>/delete/', views.delete_birthday, name='delete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
