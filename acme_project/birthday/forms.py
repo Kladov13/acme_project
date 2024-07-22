5b017e4879b704210f1901e7335f391ef06a3da2
Danil Kladov <kladov113@yandex.com>
2024-07-22 22:04:46 +0300

    Birthday

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 from django.core.exceptions import ValidationError
 
-from .models import Birthday
+from django.core.mail import send_mail
+
+from .models import Birthday, Congratulation
 
 
 BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж Харрисон', 'Ринго Старр'}
@@ -11,6 +13,7 @@ BEATLES = {'Джон Леннон', 'Пол Маккартни', 'Джордж �
 class BirthdayForm(forms.ModelForm):
     class Meta:
         model = Birthday
+        exclude = ('author',)
         fields = '__all__'
         widgets = {
             'birthday': forms.DateInput(attrs={'type': 'date'})
@@ -24,12 +27,25 @@ class BirthdayForm(forms.ModelForm):
             return first_name.split()[0]
     
     def clean(self):
-         super().clean()
-         first_name = self.cleaned_data['first_name']
-         last_name = self.cleaned_data['last_name']
-         if f'{first_name} {last_name}' in BEATLES:
-            raise ValidationError(
-                 'Мы тоже любим Битлз, но введите пожалуйста настоящее имя!'
+        super().clean()
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if f'{first_name} {last_name}' in BEATLES:
+            # Отправляем письмо, если кто-то представляется 
+            # именем одного из участников Beatles.
+            send_mail(
+                subject='Another Beatles member',
+                message=f'{first_name} {last_name} пытался опубликовать запись!',
+                from_email='birthday_form@acme.not',
+                recipient_list=['admin@acme.not'],
+                fail_silently=True,
             )
-        
-        
+            raise ValidationError(
+                'Мы тоже любим Битлз, но введите, пожалуйста, настоящее имя!'
+            ) 
+
+class CongratulationForm(forms.ModelForm):
+    
+    class Meta:
+        model = Congratulation
+        fields = ('text',) 
